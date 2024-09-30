37c4becc49e6798f46be6578f65c88d2b1b6362f
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2024-09-30 17:01:28 -0300

    update tests

--- first_app/test/buttons_test.dart
+++ first_app/test/buttons_test.dart
@@ -4,74 +4,76 @@ import 'package:provider/provider.dart';
 import 'package:first_app/main.dart';
 
 void main() {
-  //certo
-  //Next
-  testWidgets('Clicar em next gera uma nova palavra',
-      (WidgetTester tester) async {
-    // Inicializa o widget principal
-    await tester.pumpWidget(MyApp());
+  group('Testar buttons', () {
+    //certo
+    //Next
+    testWidgets('Clicar em next gera uma nova palavra',
+        (WidgetTester tester) async {
+      // Inicializa o widget principal
+      await tester.pumpWidget(MyApp());
 
-    // Obtém o texto do widget BigCard antes de apertar o botão "Next"
-    var initialWordPair = find.byType(BigCard);
+      // Obtém o texto do widget BigCard antes de apertar o botão "Next"
+      var initialWordPair = find.byType(BigCard);
 
-    // Clica no botão "Next"
-    await tester.tap(find.text('Next'));
-    await tester.pump(); // Espera que a tela seja redesenhada
+      // Clica no botão "Next"
+      await tester.tap(find.text('Next'));
+      await tester.pump(); // Espera que a tela seja redesenhada
 
-    // Verifica se a palavra gerada é diferente
-    var newWordPair = find.byType(BigCard);
-    expect(initialWordPair, isNot(newWordPair));
-  });
-  //Like
-  testWidgets('Verificar funcionamento do botão like',
-      (WidgetTester tester) async {
-    // Inicializa o widget principal
-    await tester.pumpWidget(
-      ChangeNotifierProvider(
-        create: (_) => MyAppState(),
-        child: MyApp(),
-      ),
-    );
+      // Verifica se a palavra gerada é diferente
+      var newWordPair = find.byType(BigCard);
+      expect(initialWordPair, isNot(newWordPair));
+    });
+    //Like
+    testWidgets('Verificar funcionamento do botão like',
+        (WidgetTester tester) async {
+      // Inicializa o widget principal
+      await tester.pumpWidget(
+        ChangeNotifierProvider(
+          create: (_) => MyAppState(),
+          child: MyApp(),
+        ),
+      );
 
-    await tester
-        .pumpAndSettle(); // Aguarda que a tela esteja completamente renderizada
+      await tester
+          .pumpAndSettle(); // Aguarda que a tela esteja completamente renderizada
 
-    // Verifica se o ícone é de um coração vazio (não curtido) dentro do botão "Like"
-    expect(
-      find.descendant(
-        of: find.byType(GeneratorPage), // Busca entre todos os botões
-        matching: find.byIcon(
-            Icons.favorite_border), // Verifica se o ícone é do coração vazio
-      ),
-      findsOneWidget,
-    );
+      // Verifica se o ícone é de um coração vazio (não curtido) dentro do botão "Like"
+      expect(
+        find.descendant(
+          of: find.byType(GeneratorPage), // Busca entre todos os botões
+          matching: find.byIcon(
+              Icons.favorite_border), // Verifica se o ícone é do coração vazio
+        ),
+        findsOneWidget,
+      );
 
-    // Clica no botão "Like"
-    await tester.tap(find.text('Like'));
-    await tester.pump(); // Aguarda o rebuild
+      // Clica no botão "Like"
+      await tester.tap(find.text('Like'));
+      await tester.pump(); // Aguarda o rebuild
 
-    // Verifica se o ícone mudou para um coração cheio (curtido) dentro do botão "Like"
-    expect(
-      find.descendant(
-        of: find.byType(GeneratorPage), // Busca entre todos os botões
-        matching: find
-            .byIcon(Icons.favorite), // Verifica se o ícone é do coração cheio
-      ),
-      findsOneWidget,
-    );
+      // Verifica se o ícone mudou para um coração cheio (curtido) dentro do botão "Like"
+      expect(
+        find.descendant(
+          of: find.byType(GeneratorPage), // Busca entre todos os botões
+          matching: find
+              .byIcon(Icons.favorite), // Verifica se o ícone é do coração cheio
+        ),
+        findsOneWidget,
+      );
 
-    // Clica no botão "Like" novamente para remover dos favoritos
-    await tester.tap(find.text('Like'));
-    await tester.pump(); // Aguarda o rebuild
+      // Clica no botão "Like" novamente para remover dos favoritos
+      await tester.tap(find.text('Like'));
+      await tester.pump(); // Aguarda o rebuild
 
-    // Verifica se o ícone voltou para um coração vazio (não curtido) dentro do botão "Like"
-    expect(
-      find.descendant(
-        of: find.byType(GeneratorPage), // Busca entre todos os botões
-        matching: find.byIcon(
-            Icons.favorite_border), // Verifica se o ícone é do coração vazio
-      ),
-      findsOneWidget,
-    );
+      // Verifica se o ícone voltou para um coração vazio (não curtido) dentro do botão "Like"
+      expect(
+        find.descendant(
+          of: find.byType(GeneratorPage), // Busca entre todos os botões
+          matching: find.byIcon(
+              Icons.favorite_border), // Verifica se o ícone é do coração vazio
+        ),
+        findsOneWidget,
+      );
+    });
   });
 }
--- first_app/test/testes_unitarios/favorite_test.dart
+++ first_app/test/testes_unitarios/favorite_test.dart
@@ -2,43 +2,45 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:first_app/main.dart';
 
 void main() {
-  test('alternar Favorito, adicionar e remover palavra dos favoritos', () {
-    final appState = MyAppState();
-    final word = appState.current;
+  group('Testar favoritos', () {
+    test('alternar Favorito, adicionar e remover palavra dos favoritos', () {
+      final appState = MyAppState();
+      final word = appState.current;
 
-    // Verifica que a lista de favoritos inicialmente não contém a palavra
-    expect(appState.favorites.contains(word), isFalse);
+      // Verifica que a lista de favoritos inicialmente não contém a palavra
+      expect(appState.favorites.contains(word), isFalse);
 
-    // Adiciona a palavra aos favoritos
-    appState.toggleFavorite();
+      // Adiciona a palavra aos favoritos
+      appState.toggleFavorite();
 
-    // Verifica se a palavra foi adicionada
-    expect(appState.favorites.contains(word), isTrue);
+      // Verifica se a palavra foi adicionada
+      expect(appState.favorites.contains(word), isTrue);
 
-    // Remove a palavra dos favoritos
-    appState.toggleFavorite();
+      // Remove a palavra dos favoritos
+      appState.toggleFavorite();
 
-    // Verifica se a palavra foi removida
-    expect(appState.favorites.contains(word), isFalse);
-  });
+      // Verifica se a palavra foi removida
+      expect(appState.favorites.contains(word), isFalse);
+    });
 
-  test('Adicionar mais de uma palavra aos favoritos', () {
-    final appState = MyAppState();
+    test('Adicionar mais de uma palavra aos favoritos', () {
+      final appState = MyAppState();
 
-    // Adiciona várias palavras aos favoritos
-    final word1 = appState.current;
-    appState.toggleFavorite();
-    expect(appState.favorites.contains(word1), isTrue);
+      // Adiciona várias palavras aos favoritos
+      final word1 = appState.current;
+      appState.toggleFavorite();
+      expect(appState.favorites.contains(word1), isTrue);
 
-    // Gera uma nova palavra e adiciona aos favoritos
-    appState.getNext();
-    final word2 = appState.current;
-    appState.toggleFavorite();
-    expect(appState.favorites.contains(word2), isTrue);
+      // Gera uma nova palavra e adiciona aos favoritos
+      appState.getNext();
+      final word2 = appState.current;
+      appState.toggleFavorite();
+      expect(appState.favorites.contains(word2), isTrue);
 
-    // Verifica se ambas as palavras estão na lista de favoritos
-    expect(appState.favorites.length, 2);
-    expect(appState.favorites.contains(word1), isTrue);
-    expect(appState.favorites.contains(word2), isTrue);
+      // Verifica se ambas as palavras estão na lista de favoritos
+      expect(appState.favorites.length, 2);
+      expect(appState.favorites.contains(word1), isTrue);
+      expect(appState.favorites.contains(word2), isTrue);
+    });
   });
 }
